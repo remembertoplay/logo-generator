eb7bee4acf791bb5c220d19270feffe7047f10f2
Remmelt Pit <remmelt.pit@container-solutions.com>
2015-11-24 21:11:39 +0100

    wip shape drawer

--- logo_gen.js
+++ logo_gen.js
@@ -1,12 +1,13 @@
-var LogoGen = function (sizeish) {
+var LogoGen = function (sizeish, shape) {
     this.d = sizeish;
+    this.shape = shape;
 
     var x = Math.sin(60 * (Math.PI / 180)) * this.d,
         y = this.d / 2,
-        draw = SVG('drawing').size('500', '500'),
+        draw = SVG('drawing').size('150', '150'),
         count = 0, // Counter to give the triangles an id, 0 based, from right top sloped down left
     // The top most point's offset from the edge of the canvas
-        xOff = 100,
+        xOff = 50,
         yOff = 10;
 
     console.log(this.d, x, y);
@@ -18,9 +19,15 @@ var LogoGen = function (sizeish) {
             (x + offsetX) + ',' + (d + offsetY) + ' ' +
             (offsetX + (left ? 0 : 2 * x )) + ',' + (y + offsetY);
 
-        draw.polygon(triangle).fill(color).stroke({width: 0.5}).attr({id: 'tr' + count, 'stroke-linejoin': 'bevel'});
+        draw.polygon(triangle).fill(color).stroke({width: 0.5}).attr({
+            id: 'tr' + shape + '_' + count,
+            'stroke-linejoin': 'bevel'
+        });
 
-        //draw.text('tr' + count).move(x - 20 + offsetX + (left ? 0 : 2 * x - 100), 40 + offsetY).font({ size: 10, anchor: 'middle'});
+        //draw.text('tr' + count).move(x - 20 + offsetX + (left ? 0 : 2 * x - 100), 40 + offsetY).font({
+        //    size: 10,
+        //    anchor: 'middle'
+        //});
 
         count++;
     }
@@ -43,7 +50,6 @@ var LogoGen = function (sizeish) {
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     }
-
 
     var colors = [
         [255, 204, 9],
@@ -84,40 +90,52 @@ var LogoGen = function (sizeish) {
     row(this.d, 2 * x + xOff, 2 * y + yOff, true, true);
     row(this.d, 2 * x + xOff, 4 * y + yOff, false, true);
 
-    console.log("Triangles drawn:", count);
+
+    //console.log("Triangles drawn:", count);
     if (count != 24) console.warn("Wrong number of triangles!");
 
-
     // Colors
-    var neighbors = {
-        'tr0': [1, 6], 'tr1': [0, 2], 'tr2': [1, 3, 8], 'tr3': [2, 4], 'tr4': [3, 10],
-        'tr5': [6, 12], 'tr6': [0, 5, 7], 'tr7': [6, 8, 14], 'tr8': [2, 7, 9], 'tr9': [8, 10, 16],
-        'tr10': [4, 9, 11], 'tr11': [10, 18], 'tr12': [5, 13], 'tr13': [12, 14, 19], 'tr14': [7, 13, 15],
-        'tr15': [14, 16, 21], 'tr16': [9, 15, 17], 'tr17': [16, 18, 23], 'tr18': [11, 17], 'tr19': [13, 20],
-        'tr20': [19, 21], 'tr21': [15, 20, 22], 'tr22': [21, 23], 'tr23': [17, 22]
-    };
+
+
+    var shapes = [
+        [9, 16, 17, 23, 22, 21],
+        [17, 23, 22, 21, 15, 14],
+        [6, 7, 8, 9, 15, 16],
+        [7, 8, 14, 15, 16, 17],
+        [9, 16, 17, 23, 14, 15],
+        [2, 8, 9, 10, 11, 16],
+        [5, 6, 7, 8, 14, 15],
+        [8, 9, 15, 16, 17, 18],
+        [6, 7, 13, 14, 15, 16, 19],
+        [2, 3, 6, 7, 8, 9, 16],
+        [1, 2, 8, 9, 15, 16, 17],
+        [2, 7, 8, 9, 16, 17, 23],
+        [9, 10, 15, 16, 17, 21],
+        [7, 12, 13, 14, 15, 16],
+        [2, 7, 8, 9, 10, 16],
+        [6, 7, 8, 13, 14, 15],
+        [9, 10, 11, 14, 15, 16],
+        [9, 13, 14, 15, 16, 19],
+        [9, 10, 13, 14, 15, 16],
+        [7, 8, 9, 10, 13, 14],
+        [7, 8, 9, 13, 14, 15],
+        [7, 9, 13, 14, 15, 16],
+        [8, 9, 10, 14, 15, 16],
+        [2, 7, 8, 9, 14, 16],
+        [8, 9, 14, 15, 16, 17],
+        [2, 3, 6, 7, 8, 9],
+        [7, 13, 14, 15, 16, 19]
+    ];
 
     function setColor(id, color) {
-        document.getElementById(id).setAttribute('fill', color);
+        document.getElementById('tr' + shape + '_' + id).setAttribute('fill', color);
     }
 
-    function doColor(color) {
-        var nr = getRandomInt(8, 12), curr = getRandomInt(0, 24), result = [], safety = 0;
-        result.push(curr);
-
-        console.log("nr", nr);
-        do {
-            safety++;
-            var n = neighbors['tr' + curr];
-            curr = n[getRandomInt(0, n.length)];
-            result.push(curr);
-        } while (result.length < nr && safety < 100);
-        if (safety > 40) console.warn(safety);
-
-        result.forEach(function (el) {
-            setColor('tr' + el, color);
+    function doColor(shape, color) {
+        console.log(shape, color);
+        shape.forEach(function (el) {
+            setColor(el, color);
         });
-        return result;
     }
 
     function intersect(a, b) {
@@ -130,27 +148,31 @@ var LogoGen = function (sizeish) {
 
     function clearColors() {
         for (var r = 0; r < 24; r++) {
-            setColor('tr' + r, 'none');
+            setColor(r, 'none');
         }
     }
 
     this.redraw = function () {
         clearColors();
-        var colors = getRandomColor();
-        console.log("colors", colors);
-
-        var set1 = doColor(colors.c1);
-        var set2 = doColor(colors.c2);
-        var set3 = intersect(set1, set2);
-        set3.forEach(function (el) {
-            setColor('tr' + el, colors.ca);
-        });
+        doColor(shapes[this.shape], 'lightblue');
+        //var colors = getRandomColor();
+        //console.log("colors", colors);
+        //
+        //var set1 = doColor(colors.c1);
+        //var set2 = doColor(colors.c2);
+        //var set3 = intersect(set1, set2);
+        //set3.forEach(function (el) {
+        //    setColor('tr' + el, colors.ca);
+        //});
     };
 
 };
 
-function logo() {
-    lg.redraw();
+function logo(logo) {
+    logo.redraw();
 }
-var lg = new LogoGen(80);
-lg.redraw();
+
+
+for (var t = 0; t < 100; t++) {
+    logo(new LogoGen(30, t));
+}
